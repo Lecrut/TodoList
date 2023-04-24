63e4d49e1b005290c5fda58760b3548480159cfb
Lecrut <filip.lecrut12@gmail.com>
2023-04-24 23:34:45 +0200

    dodawanie zadań

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,34 @@
-<script setup>
-  
+<script >
+  let id = 0;
 
+  export default {
+    data() {
+      return {
+        newTodo: "",
+        hideCompleted: false,
+        todos: []
+      }
+    },
+    computed : {
+      //...
+    },
+    methods: {
+      addTodo() {
+        this.todos.push({id:id++, text: this.newTodo, done: false})
+        this.newTodo = ''
+        console.log(this.todos);
+      }
+    }
+  }
+  
 </script>
 
 <template>
   <h1>Lista zadań</h1>
   <main>
-
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo">
+      <button>Dodaj</button>
+    </form>
   </main>
 </template>
