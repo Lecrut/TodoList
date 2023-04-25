f43e4f951a0f132a780eeebc3ca701be17f0c90d
Lecrut <filip.lecrut12@gmail.com>
2023-04-25 11:45:52 +0200

    edycja zadań

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script >
+<script>
   let id = 0;
 
   export default {
@@ -14,11 +14,20 @@
     },
     methods: {
       addTodo() {
-        this.todos.push({id:id++, text: this.newTodo, done: false})
+        this.todos.push({id:id++, text: this.newTodo, done: false, edit: false})
         this.newTodo = ''
       },
       removeTodo(todo) {
         this.todos = this.todos.filter((t)=> t !==todo)
+      },
+      editTodo(todo) {
+        todo.edit = true
+        this.value = todo.text
+      },
+      changeTodo(todo, value) {
+        todo.text = value
+        todo.edit = false
+        this.value = ''
       }
     }
   }
@@ -35,8 +44,11 @@
     <ul>
     <li v-for="todo in todos" :key="todo.id">
       <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <span :class="{ done: todo.done }" v-show="!todo.edit">{{ todo.text }}</span>
+      <input type="text" v-model="todo.text" v-show="todo.edit">
       <button @click="removeTodo(todo)">X</button>
+      <button v-show="!todo.edit" @click="editTodo(todo)">Edit</button>
+      <button v-show="todo.edit" @click="changeTodo(todo, todo.text)">Save</button>
     </li>
   </ul>
   </main>
