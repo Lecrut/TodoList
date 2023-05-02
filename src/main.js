a4f142720fb23238bac9b1b5a5fc585bc9129b99
Lecrut <filip.lecrut12@gmail.com>
2023-05-02 11:51:26 +0200

    dzialajcy localhost

--- src/main.js
+++ src/main.js
@@ -3,10 +3,24 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createI18n } from 'vue-i18n'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+const i18n = createI18n({
+  locale: 'pl',
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(i18n)
 
 app.mount('#app')
